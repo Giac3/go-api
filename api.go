94452489ecffd7183d09b575d1098671e05e4754
Giacomo Pessotto <giacomopesso@gmail.com>
2023-08-27 16:31:57 +0100

    add image generation endpoint

--- api.go
+++ api.go
@@ -11,28 +11,10 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var API_TOKEN string = ""
-
-func getChatCompletion(msg interface{}) (*http.Response, error) {
-	jsonBody, err := json.Marshal(msg)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(jsonBody))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+API_TOKEN)
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
+var imageSizes = map[string]struct{}{
+	"256x256":   {},
+	"512x512":   {},
+	"1024x1024": {},
 }
 
 type Message struct {
@@ -48,6 +30,14 @@ type ChatCompletion struct {
 	Max_tokens        *int      `json:"max_tokens,omitempty"`
 	Frequency_penalty float32   `json:"frequency_penalty,omitempty"`
 	Presence_penalty  float32   `json:"presence_penalty,omitempty"`
+}
+
+type GenerateImage struct {
+	Prompt          string `json:"prompt"`
+	N               int    `json:"n,omitempty"`
+	Size            string `json:"size,omitempty"`
+	Response_format string `json:"response_format,omitempty"`
+	User            string `json:"user,omitempty"`
 }
 
 type OpenAIChatResponse struct {
@@ -70,6 +60,93 @@ type OpenAIChatResponse struct {
 	} `json:"usage"`
 }
 
+type OpenAIImageResponse struct {
+	Created int64 `json:"created"`
+	Data    []struct {
+		Url string `json:"url"`
+	}
+}
+
+var API_TOKEN string = ""
+
+func getChatCompletion(chatCompletion ChatCompletion) (*http.Response, error) {
+	jsonBody, err := json.Marshal(chatCompletion)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+API_TOKEN)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func getImageGeneration(generateImage GenerateImage) (*http.Response, error) {
+	jsonBody, err := json.Marshal(generateImage)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "https://api.openai.com/v1/images/generations", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+API_TOKEN)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func setDefaultValuesChat(chatCompletion *ChatCompletion) {
+	if chatCompletion.Model == "" {
+		chatCompletion.Model = "gpt-3.5-turbo"
+	}
+
+	if chatCompletion.Temperature == nil {
+		temp := float32(0.1)
+		chatCompletion.Temperature = &temp
+	}
+	if chatCompletion.Top_p == nil {
+		temp := float32(1)
+		chatCompletion.Top_p = &temp
+	}
+	if chatCompletion.Max_tokens == nil {
+		temp := int(1000)
+		chatCompletion.Max_tokens = &temp
+	}
+}
+func setDefaultValuesImage(generateImage *GenerateImage) {
+	if generateImage.Prompt == "" {
+		generateImage.Prompt = "something random"
+	}
+	if generateImage.Response_format == "" {
+		generateImage.Response_format = "url"
+	}
+	if generateImage.N == 0 {
+		generateImage.N = 1
+	}
+	_, validSize := imageSizes[generateImage.Size]
+
+	if generateImage.Size == "" || !validSize {
+		generateImage.Size = "256x256"
+	}
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -82,42 +159,24 @@ func main() {
 	app.GET("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Hello, go to /docs to check out the endpoints")
 	})
+	app.GET("/docs", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello docs are coming soon")
+	})
 
 	app.POST("/chatCompletion", func(res http.ResponseWriter, req *http.Request) {
 
 		decoder := json.NewDecoder(req.Body)
 		var chatCompletion ChatCompletion
 		err := decoder.Decode(&chatCompletion)
-
-		if chatCompletion.Model == "" {
-			chatCompletion.Model = "gpt-3.5-turbo"
-		}
-
-		if chatCompletion.Temperature == nil {
-			temp := float32(0.1)
-			chatCompletion.Temperature = &temp
-		}
-		if chatCompletion.Top_p == nil {
-			temp := float32(1)
-			chatCompletion.Top_p = &temp
-		}
-		if chatCompletion.Max_tokens == nil {
-			temp := int(1000)
-			chatCompletion.Max_tokens = &temp
-		}
-
-		if len(chatCompletion.Messages) == 0 {
-			http.Error(res, "Error: At least one message is required", http.StatusBadRequest)
-			return
-		}
-
-		if len(chatCompletion.Messages) == 0 {
-			http.Error(res, "Error: At least one message is required", http.StatusBadRequest)
-			return
-		}
-
 		if err != nil {
 			http.Error(res, "Error decoding request body", http.StatusBadRequest)
+			return
+		}
+
+		setDefaultValuesChat(&chatCompletion)
+
+		if len(chatCompletion.Messages) == 0 {
+			http.Error(res, "Error: At least one message is required", http.StatusBadRequest)
 			return
 		}
 
@@ -143,6 +202,34 @@ func main() {
 		}
 
 		http.Error(res, "No choices in OpenAI response", http.StatusInternalServerError)
+	})
+
+	app.POST("/generateImage", func(res http.ResponseWriter, req *http.Request) {
+		decoder := json.NewDecoder(req.Body)
+		var generateImage GenerateImage
+		decoder.Decode(&generateImage)
+		setDefaultValuesImage(&generateImage)
+
+		resp, err := getImageGeneration(generateImage)
+		if err != nil {
+			http.Error(res, "Error generating Image", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var aiRes OpenAIImageResponse
+		err = json.NewDecoder(resp.Body).Decode(&aiRes)
+		if err != nil {
+			http.Error(res, "Error reading OpenAI response", http.StatusInternalServerError)
+			return
+		}
+		if len(aiRes.Data) > 0 {
+			res.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(res).Encode(aiRes.Data)
+			return
+		}
+
+		http.Error(res, "No Images in OpenAI response", http.StatusInternalServerError)
 	})
 
 	fmt.Println("Server is live on localhost:3033")
